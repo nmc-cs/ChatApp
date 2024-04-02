bfa1061cdee63714eee088af551ec918b06d4c36
Nitin Chava <nitinchava10@gmail.com>
2024-04-01 21:01:10 -0500

    modified ChatsPage for both chat ui

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,19 +1,31 @@
+import React from "react";
+
 import {
-  MultiChatSocket,
-  MultiChatWindow,
-  useMultiChatLogic,
-} from "react-chat-engine-advanced";
+  PrettyChatWindow,
+  // these 3 imports are for react-chat-engne-advanced
+  //   MultiChatSocket,
+  //   MultiChatWindow,
+  //   useMultiChatLogic,
+} from "react-chat-engine-pretty";
 
 const ChatsPage = (props) => {
-  const chatProps = useMultiChatLogic(
-    "30e87589-14c5-4ade-b38d-29cd1dda5458",
-    props.user.username,
-    props.user.secret
-  );
+  // code for react-chat-engine-advanced
+  //   const chatProps = useMultiChatLogic(
+  //     "30e87589-14c5-4ade-b38d-29cd1dda5458",
+  //     props.user.username,
+  //     props.user.secret
+  //   );
   return (
     <div style={{ height: "100vh" }}>
-      <MultiChatSocket {...chatProps} />
-      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+      {/* code for react-chat-engine-advanced */}
+      {/* <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} /> */}
+      <PrettyChatWindow
+        projectId="30e87589-14c5-4ade-b38d-29cd1dda5458"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
     </div>
   );
 };
